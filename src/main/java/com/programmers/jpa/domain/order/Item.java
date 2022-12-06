75b4656c4c3770c007bb1fda911e74c1ad8a4919
goseungwon <knmy0925@gmail.com>
2022-12-07 00:18:22 +0900

    고급 매핑 - 상속(single_table)

--- src/main/java/com/programmers/jpa/domain/order/Item.java
+++ src/main/java/com/programmers/jpa/domain/order/Item.java
@@ -8,7 +8,8 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
 public abstract class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
